a7d29ed9358be11afe525982b351db4ca0cfd790
Amanda Bertsch <abertsch72@gmail.com>
2019-12-06 09:07:39 -0700

    vocally does not like to be closed

--- src/ohno/OhNo.java
+++ src/ohno/OhNo.java
@@ -1,17 +1,27 @@
 package ohno;
 import javafx.application.*;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.geometry.Rectangle2D;
 import javafx.stage.Screen;
+import javafx.scene.paint.*;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane; 
+
 
 public class OhNo extends Application {
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
 		primaryStage.initStyle(StageStyle.UNDECORATED);
-		primaryStage.setAlwaysOnTop(true);		
-		primaryStage.setOnCloseRequest((event)->{event.consume();});
+		primaryStage.setAlwaysOnTop(true);
+		Label whoops = new Label("Hey! Don't do that! :(((");
+		BorderPane bp = new BorderPane();
+		bp.setCenter(whoops);
+		Scene s = new Scene(bp);
+		whoops.setTextFill(Color.RED);
+		primaryStage.setOnCloseRequest((event)->{primaryStage.setScene(s);event.consume();});
 		Rectangle2D primaryScreenBounds = Screen.getPrimary().getBounds();
 
         //set Stage boundaries to visible bounds of the main screen
